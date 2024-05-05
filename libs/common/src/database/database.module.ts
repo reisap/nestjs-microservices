ecb2b3eeffdf95074dec674ea8c9af32d188966b
Reisa Prasaptaraya <r.prasaptaraya@gmail.com>
2024-05-05 09:16:48 +0700

    [etc] add pretier for better coding

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,8 +1,16 @@
 import {Module} from "@nestjs/common"
+import {ConfigService} from "@nestjs/config"
 import {MongooseModule} from "@nestjs/mongoose"
 
 @Module({
-    imports: [MongooseModule.forRoot("mongodb://db:27017/sleepr")],
+    imports: [
+        MongooseModule.forRootAsync({
+            useFactory: (configService: ConfigService) => ({
+                uri: configService.get("MONGODB_URI"),
+            }),
+            inject: [ConfigService],
+        }),
+    ],
 })
 export class DatabaseModule {}
 
